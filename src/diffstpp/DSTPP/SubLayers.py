f5d31b273be03644ca95b533455262fc501d9386
YahyaAalaila <148972284+YahyaAalaila@users.noreply.github.com>
2025-07-24 10:01:28 +0200

    Update SubLayers.py

--- src/diffstpp/DSTPP/SubLayers.py
+++ src/diffstpp/DSTPP/SubLayers.py
@@ -1,10 +1,8 @@
-import numpy as np
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import DSTPP.Constants as Constants
-from DSTPP.Modules import ScaledDotProductAttention
+
+from .Modules import ScaledDotProductAttention
 
 
 class MultiHeadAttention(nn.Module):
